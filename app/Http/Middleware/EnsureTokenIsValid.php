61438803e3c41e2e9a688e7d292b04cd7b367453
FrancisPogi <cabusasfg779@gmail.com>
2023-03-08 13:30:30 +0800

    Update CDRRMO

---
+++ app/Http/Middleware/EnsureTokenIsValid.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace App\Http\Middleware;
+ 
+use Closure;
+ 
+class EnsureTokenIsValid
+{
+    public function handle($request, Closure $next)
+    {
+        if ($request->input('token') !== 'my-secret-token') {
+            return redirect('home');
+        }
+ 
+        return $next($request);
+    }
+}
