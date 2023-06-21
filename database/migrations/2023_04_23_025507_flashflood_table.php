53723ec1f3e0aec10d737e2ff56959362277b1c1
margallostephen <spmargallo@gmail.com>
2023-06-21 14:51:39 +0800

    Update typhoon and flashflood column

--- database/migrations/2023_04_23_025507_flashflood_table.php
+++ database/migrations/2023_04_23_025507_flashflood_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('flashflood', function (Blueprint $table) {
             $table->id();
             $table->string('location');
+            $table->string('status')->default('Rising');
             $table->foreignId('disaster_id')->references('id')->on('disaster')->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
