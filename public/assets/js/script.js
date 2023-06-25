65e0c56a32a5b1b78a1f4a5679ad6f68d3e746a4
margallostephen <spmargallo@gmail.com>
2023-06-26 03:59:45 +0800

    Add confirm and message modal

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,14 +3,41 @@ let mobile_side_button = document.querySelector('#btn-sidebar-mobile');
 let mobile_side_close = document.querySelector('#btn-sidebar-close');
 let sidebar = document.querySelector('.sidebar');
 
-btn.onclick = function(){
+btn.onclick = function () {
     sidebar.classList.toggle("active");
 }
 
-mobile_side_button.onclick = function (){
+mobile_side_button.onclick = function () {
     sidebar.classList.toggle("active");
 }
 
-mobile_side_close.onclick = function (){
+mobile_side_close.onclick = function () {
     sidebar.classList.remove("active");
+}
+
+function confirmModal(text) {
+    return Swal.fire({
+        title: 'Confirmation',
+        text: text,
+        icon: 'info',
+        iconColor: '#1d4ed8',
+        showDenyButton: true,
+        confirmButtonText: 'Yes',
+        confirmButtonColor: '#15803d',
+        denyButtonText: 'No',
+        denyButtonColor: '#B91C1C',
+        allowOutsideClick: false,
+    });
+}
+
+function messageModal(title, text, icon, iconColor) {
+    return Swal.fire({
+        title: title,
+        text: text,
+        icon: icon,
+        iconColor: iconColor,
+        showConfirmButton: false,
+        timer: 2000,
+        allowOutsideClick: false,
+    });
 }
