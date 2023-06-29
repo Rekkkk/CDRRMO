ac2ba4413f313e2f582aefda2232620724f07701
FrancisPogi <cabusasfg779@gmail.com>
2023-06-29 10:59:11 +0800

    Update CDRRMO

--- resources/views/userpage/guideline/addGuide.blade.php
+++ resources/views/userpage/guideline/addGuide.blade.php
@@ -1,31 +1,26 @@
 <div class="modal fade" id="createGuideModal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header bg-red-700">
-                <h1 class="modal-title fs-5 text-center text-white">Guide Form</h1>
+            <div class="modal-header bg-green-600">
+                <h1 class="modal-title fs-5 text-center text-white">Create Guide Form</h1>
             </div>
             <div class="modal-body">
-                <form id="createGuideForm" name="createGuideForm">
+                <form id="createGuideForm">
                     @csrf
-                    <input type="hidden" name="create_guide_id">
+                    <input type="text" name="create_guide_id" hidden>
                     <div class="mb-3">
                         <label for="label" class="flex items-center justify-center">Guide
                             Description</label>
                         <input type="text" name="label" class="form-control" autocomplete="off"
                             placeholder="Enter Guide Description">
-                        <span class="text-danger error-text label_error"></span>
                     </div>
                     <div class="mb-3">
                         <label for="content" class="flex items-center justify-center">Guide Content</label>
                         <textarea name="content" class="form-control" autocomplete="off" placeholder="Enter Guide Content" rows="5"></textarea>
-                        <span class="text-danger error-text content_error"></span>
                     </div>
                     <div class="modal-footer text-white">
-                        <button type="button"
-                            class="bg-slate-600 p-2 rounded drop-shadow-lg hover:bg-slate-700"
-                            data-bs-dismiss="modal">Close</button>
                         <button id="submitGuideBtn"
-                            class="bg-red-700 p-2 rounded drop-shadow-lg hover:bg-red-800">Post
+                            class="btn-submit p-2">Post
                             Guide</button>
                     </div>
                 </form>
