0c91c2d81764f52b731fc442064d2b08d97e87ee
FrancisPogi <cabusasfg779@gmail.com>
2023-07-19 19:36:20 +0800

    Remove Models

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Typhoon;
 use App\Models\Evacuee;
 use App\Models\Disaster;
-use App\Models\Flashflood;
 use Illuminate\Http\Request;
 use App\Models\EvacuationCenter;
 use App\Exports\EvacueeDataExport;
@@ -22,12 +20,9 @@ class MainController extends Controller
     }
     public function dashboard()
     {
-        $evacuee = new Evacuee;
-        $disaster = new Disaster;
-        
-        $onGoingDisaster = $disaster->where('status', "On Going")->get();
+        $onGoingDisaster = Disaster::where('status', "On Going")->get();
         $activeEvacuation = $this->evacuationCenter->where('status', 'Active')->count();
-        $inEvacuationCenter = $evacuee->whereNull('date_out')->count();
+        $inEvacuationCenter = Evacuee::whereNull('date_out')->count();
 
         return view('userpage.dashboard',  compact('activeEvacuation', 'inEvacuationCenter', 'onGoingDisaster'));
     }
@@ -39,22 +34,10 @@ class MainController extends Controller
 
     public function manageEvacueeInformation(Request $request)
     {
-        $disaster = new Disaster;
-
+        $disasterList = Disaster::where('status', 'On Going')->get();
         $evacuationList = $this->evacuationCenter->all();
-        $typhoonList =  Typhoon::all();
-        $flashfloodList = Flashflood::all()->where('status', 'Rising');
-        $disasterList = null;
 
-        if ($typhoonList->isNotEmpty() && $flashfloodList->isNotEmpty()) {
-            $disasterList = $disaster->all();
-        } else if ($typhoonList->isNotEmpty()) {
-            $disasterList = $disaster->where('id', 1)->get();
-        } else if ($flashfloodList->isNotEmpty()) {
-            $disasterList = $disaster->where('id', 2)->get();
-        }
-
-        return view('userpage.evacuee.evacuee', compact('evacuationList', 'disasterList', 'typhoonList', 'flashfloodList'));
+        return view('userpage.evacuee.evacuee', compact('evacuationList', 'disasterList'));
     }
 
     public function evacuationCenterLocator()
