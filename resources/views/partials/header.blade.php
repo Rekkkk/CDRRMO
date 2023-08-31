7a89f9d4bea4619c6cddab3312b50fdb85701d35
FrancisPogi <cabusasfg779@gmail.com>
2023-08-31 19:04:01 +0800

    Fix dropdown Item

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,8 +12,8 @@
                 @auth
                     <li><a class="changePasswordBtn dropdown-item" href="#changePasswordModal" data-bs-toggle="modal">
                             <i class="bi bi-shield-lock"></i>Change Password</a></li>
-                    <li><a class="myProfile dropdown-item" href="{{ route('account.display.profile') }}">
-                            <i class="bi bi-person-circle"></i>My Profile</a></li>
+                    <li><a class="myAccount dropdown-item" href="{{ route('account.display.profile') }}">
+                            <i class="bi bi-person-circle"></i>My Account</a></li>
                     <li id="logoutBtn"><a class="logout dropdown-item" href="{{ route('logout.user') }}">
                             <i class="bi bi-box-arrow-in-left"></i>Logout</a>
                     </li>
